6e1e65c7c550002dcc5e38adce291013a5bf34c3
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-05-09 22:51:11 +0200

    creating a getSize file function

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 
-
-//path to work with
-
-//const currentPath = path.join(path.dirname(__dirname) + "/sortingfilesfolder/testZone/");
-
 //It creates a folder pattern "extensionFile + Files" and move this file into its folder
 
 function handlingFiles(currentPath) {
@@ -78,11 +73,12 @@ function filesForFolder(array, callback) {
     })
 }
 
-//It executes the functions
 
+//fetching path from terminal
 
 const clientPath = process.argv[2];
-//console.log("clientPath:" + clientPath);
+
+//adjusting the path
 
 const currentPath = path.join(path.dirname(__dirname) + clientPath);
 
@@ -93,12 +89,32 @@ if (!fs.existsSync(currentPath)) {
     (async () => {
         const newFoldersCreated = await handlingFiles(currentPath)
         if (newFoldersCreated.length === 0) {
-            console.clear();
+
             return console.log(chalk.yellow("No hay archivos que organizar."));
         }
         await filesForFolder(newFoldersCreated, (data) => {
+            //console.log(chalk.green('data: ' + JSON.stringify(data)));
             console.clear();
-            console.log(chalk.green('data: ' + JSON.stringify(data)));
+            console.log(chalk.inverse.green('LISTANDO CARPETAS CREADAS'));
+
+            var out = data.map(function (item) {
+                return {
+                    folderName: item.folderName,
+                    numberFiles: item.numberFiles
+                };
+            });
+            console.table(out);
         })
     })()
 }
+
+
+// function getSize(file) {
+//     const stats = fs.statSync(file);
+//     const fileSizeInBytes = stats.size;
+//     //Convert the file size to megabytes (optional)
+//     const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+//     return console.log('tamaño: ' + fileSizeInMegabytes + " Mb");
+// }
+
+// getSize("app.js")
