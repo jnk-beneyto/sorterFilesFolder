c435ead2f1dd6c7c93d1acef069243ef9f9926d4
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-05-09 20:36:00 +0200

    modifying all functions

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
 const path = require('path');
 const fs = require('fs');
 
-const newFoldersCreated = [];
+//path to work with
 
 const currentPath = path.join(path.dirname(__dirname) + "/sortingfilesfolder/testZone/");
 
-console.log(currentPath);
+//It creates a folder pattern "extensionFile + Files" and move this file into its folder
 
-//listing root project files
-fs.readdirSync(currentPath).forEach(file => {
-    const isFile = fs.lstatSync(currentPath + file).isFile() ? true : false; //checking if is a file
+function handlingFiles(currentPath) {
+    const newFoldersCreated = [];
+    fs.readdirSync(currentPath).forEach(file => {
+        const isFile = fs.lstatSync(currentPath + file).isFile() ? true : false; //checking if is a file
 
-    if (isFile) {
-        const ext = (path.extname(file)).slice(1); //removing dot from extension
-        const folderName = ext + "Files";
+        if (isFile) {
+            const ext = (path.extname(file)).slice(1); //removing dot from extension
+            const folderName = ext + "Files";
 
-        const newPath = path.join(currentPath, folderName);
+            const newPath = path.join(currentPath, folderName);
 
-        //checking if extension folder exists otherwise create it
-        if (!fs.existsSync(newPath)) {
+            //checking if extension folder exists otherwise create it
+            if (!fs.existsSync(newPath)) {
 
-            newFoldersCreated.push(folderName);
+                newFoldersCreated.push(folderName);
 
-            fs.mkdirSync(newPath, {
-                recursive: true
-            }, (err) => {
+                fs.mkdirSync(newPath, {
+                    recursive: true
+                }, (err) => {
+                    if ((err) => console.log(err));
+                });
+            }
+
+            const oldPathAndFile = currentPath + "/" + file;
+            const newPathAndFile = newPath + "/" + file;
+
+            fs.rename(oldPathAndFile, newPathAndFile, function (err) {
                 if ((err) => console.log(err));
             });
         }
+    });
+    return newFoldersCreated;
+}
 
-        const oldPathAndFile = currentPath + "/" + file;
-        const newPathAndFile = newPath + "/" + file;
-
-        fs.rename(oldPathAndFile, newPathAndFile, function (err) {
-            if ((err) => console.log(err));
-            console.log(`archivo ${file} movido a / ${folderName}`);
-        });
-    }
-
-
-});
-
+// count the files about the folder 
 
 function countingFiles(path) {
-    fs.readdir(path, function (error, files) {
-        var totalFiles = files.length; // return the number of files
-        console.log(totalFiles); // print the total number of files 
+    return new Promise(function (resolve, reject) {
+        fs.readdir(path, function (error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result.length); //return number of files
+            }
+        });
     });
 }
 
+//it creates and array of objects like : [{"folderName":"jpegFiles","numberFiles":2},{"folderName":"textFiles","numberFiles":3}]
 
-newFoldersCreated.forEach((x) => {
+function filesForFolder(array, callback) {
+    const arrNumberFilesFolders = [];
+    array.forEach(async (nameFolder, indexFile) => {
+        newFolderPath = currentPath + "/" + nameFolder;
+        const totalFiles = await countingFiles(newFolderPath);
 
-    newFolderPath = currentPath + "/" + x;
-    countingFiles(newFolderPath)
-    //console.log(` carpeta ${x} creada con ${totFiles} archivos`);
+        var obj = {};
+        obj["folderName"] = nameFolder;
+        obj["numberFiles"] = totalFiles;
 
-});
+        arrNumberFilesFolders.push(obj)
+        const index = indexFile >= array.length - 1;
+        if (index) {
+            callback(arrNumberFilesFolders);
+        }
+    })
+}
+
+//It executes the functions
+
+(async () => {
+    const newFoldersCreated = await handlingFiles(currentPath)
+    await filesForFolder(newFoldersCreated, (data) => {
+        console.log('data: ' + JSON.stringify(data));
+    })
+})()
