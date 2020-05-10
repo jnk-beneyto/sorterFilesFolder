be3855d79f72fe0bb377771c45933576a237bf4e
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-05-10 10:27:42 +0200

    adding utils projects and moving functions into it

--- app.js
+++ app.js
@@ -1,77 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
-
-
-//It creates a folder pattern "extensionFile + Files" and move this file into its folder
-
-function handlingFiles(currentPath) {
-    const newFoldersCreated = [];
-    fs.readdirSync(currentPath).forEach(file => {
-        const isFile = fs.lstatSync(currentPath + file).isFile() ? true : false; //checking if is a file
-
-        if (isFile) {
-            const ext = (path.extname(file)).slice(1); //removing dot from extension
-            const folderName = ext + "Files";
-
-            const newPath = path.join(currentPath, folderName);
-
-            //checking if extension folder exists otherwise create it
-            if (!fs.existsSync(newPath)) {
-
-                newFoldersCreated.push(folderName);
-
-                fs.mkdirSync(newPath, {
-                    recursive: true
-                }, (err) => {
-                    if ((err) => console.log(err));
-                });
-            }
-
-            const oldPathAndFile = currentPath + "/" + file;
-            const newPathAndFile = newPath + "/" + file;
-
-            fs.rename(oldPathAndFile, newPathAndFile, function (err) {
-                if ((err) => console.log(err));
-            });
-        }
-    });
-    return newFoldersCreated;
-}
-
-// count the files about the folder 
-
-function countingFiles(path) {
-    return new Promise(function (resolve, reject) {
-        fs.readdir(path, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result.length); //return number of files
-            }
-        });
-    });
-}
-
-//it creates and array of objects like : [{"folderName":"jpegFiles","numberFiles":2},{"folderName":"textFiles","numberFiles":3}]
-
-function filesForFolder(array, callback) {
-    const arrNumberFilesFolders = [];
-    array.forEach(async (nameFolder, indexFile) => {
-        newFolderPath = currentPath + "/" + nameFolder;
-        const totalFiles = await countingFiles(newFolderPath);
-
-        var obj = {};
-        obj["folderName"] = nameFolder;
-        obj["numberFiles"] = totalFiles;
-
-        arrNumberFilesFolders.push(obj)
-        const index = indexFile >= array.length - 1;
-        if (index) {
-            callback(arrNumberFilesFolders);
-        }
-    })
-}
+const {
+    filesForFolder,
+    handlingFiles,
+    countingFiles
+} = require('./utils/utils')
 
 
 //fetching path from terminal
@@ -92,7 +26,7 @@ if (!fs.existsSync(currentPath)) {
 
             return console.log(chalk.yellow("No hay archivos que organizar."));
         }
-        await filesForFolder(newFoldersCreated, (data) => {
+        await filesForFolder(currentPath, newFoldersCreated, (data) => {
             //console.log(chalk.green('data: ' + JSON.stringify(data)));
             console.clear();
             console.log(chalk.inverse.green('LISTANDO CARPETAS CREADAS'));
---
+++ utils/utils.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const fs = require('fs');
+
+//It creates a folder pattern "extensionFile + Files" and move this file into its folder
+
+function handlingFiles(currentPath) {
+    const newFoldersCreated = [];
+    fs.readdirSync(currentPath).forEach(file => {
+        const isFile = fs.lstatSync(currentPath + file).isFile() ? true : false; //checking if is a file
+
+        if (isFile) {
+            const ext = (path.extname(file)).slice(1); //removing dot from extension
+            const folderName = ext + "Files";
+
+            const newPath = path.join(currentPath, folderName);
+
+            //checking if extension folder exists otherwise create it
+            if (!fs.existsSync(newPath)) {
+
+                newFoldersCreated.push(folderName);
+
+                fs.mkdirSync(newPath, {
+                    recursive: true
+                }, (err) => {
+                    if ((err) => console.log(err));
+                });
+            }
+
+            const oldPathAndFile = currentPath + "/" + file;
+            const newPathAndFile = newPath + "/" + file;
+
+            fs.rename(oldPathAndFile, newPathAndFile, function (err) {
+                if ((err) => console.log(err));
+            });
+        }
+    });
+    return newFoldersCreated;
+}
+
+// count the files about the folder 
+
+function countingFiles(path) {
+    return new Promise(function (resolve, reject) {
+        fs.readdir(path, function (error, result) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(result.length); //return number of files
+            }
+        });
+    });
+}
+
+//it creates and array of objects like : [{"folderName":"jpegFiles","numberFiles":2},{"folderName":"textFiles","numberFiles":3}]
+
+function filesForFolder(currentPath, array, callback) {
+    const arrNumberFilesFolders = [];
+    array.forEach(async (nameFolder, indexFile) => {
+        newFolderPath = currentPath + "/" + nameFolder;
+        const totalFiles = await countingFiles(newFolderPath);
+
+        var obj = {};
+        obj["folderName"] = nameFolder;
+        obj["numberFiles"] = totalFiles;
+
+        arrNumberFilesFolders.push(obj)
+        const index = indexFile >= array.length - 1;
+        if (index) {
+            callback(arrNumberFilesFolders);
+        }
+    })
+}
+
+module.exports = {
+    handlingFiles: handlingFiles,
+    countingFiles: countingFiles,
+    filesForFolder: filesForFolder
+}
